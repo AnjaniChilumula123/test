0ff2eb22759fba7a2e8335af369d8c961a4c3854
Anjani Chilumula <anjanichilumula123@gmail.com>
2022-07-09 13:18:25 +0530

    this is for testing purpose

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import{MatToolbarModule} from '@angular/material/toolbar';
+import{MatSidenavModule} from '@angular/material/sidenav';
+import{MatButtonModule} from '@angular/material/button';
+import{MatIconModule} from '@angular/material/icon';
+import{MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
